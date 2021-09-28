0f8855bc9faa25052c50285dd92a620194fbc911
espen <espenbfo@stud.ntnu.no>
2021-09-28 19:52:26 +0200

    center start button

--- application/application.py
+++ application/application.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame import midi
 
+
 def main():
     freq = 44100  # audio CD quality
     bitsize = -16  # unsigned 16 bit
@@ -11,10 +12,10 @@ def main():
     pygame.mixer.music.set_volume(0.8)
     try:
         pygame.mixer.music.load("application/HotelCalifornia.mid")
-    
+
     except:
         pass
-    
+
     pygame.mixer.music.play()
     pygame.init()
 
@@ -22,32 +23,36 @@ def main():
 
     running = True
     width = screen.get_width()
-  
-# stores the height of the
-# screen into a variable
     height = screen.get_height()
-    color_dark = (100,100,100)
+
+    # stores the height of the
+    # screen into a variable
+    start_button = pygame.image.load('imgs/start_button.png')
+    start_button_width = 200
+    start_button_height = 100
+    start_button_x = int(width / 2 - start_button_width/2)
+    start_button_y = int(height / 2 - start_button_height/2)
+
+    color_dark = (100, 100, 100)
     filePlaying = True
     while running:
         screen.fill((255, 255, 255))
-        pygame.draw.rect(screen, color_dark, [width/2,height*0.9,140,40])
+        screen.blit(start_button, (start_button_x, start_button_y))
         for event in pygame.event.get():
 
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if width/2 <= mouse[0] <= width/2+140 and height*0.9 <= mouse[1] <= height*0.9+40:
+                if start_button_x <= mouse[0] <= start_button_x + start_button_width and start_button_y <= mouse[
+                    1] <= start_button_y + start_button_height:
                     if filePlaying:
-                        pygame.mixer.music.pause() 
+                        pygame.mixer.music.pause()
                         filePlaying = False
                     else:
                         pygame.mixer.music.unpause()
-                        filePlaying = True 
+                        filePlaying = True
                 pass
-        mouse = pygame.mouse.get_pos()       
-
-       
-
+        mouse = pygame.mouse.get_pos()
 
         pygame.display.flip()
 
