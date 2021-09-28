f3710cec9811aea64e15ff2754ddfa1ea65b161a
Theodor Forgaard <tforgaard@gmail.com>
2021-09-28 19:01:30 +0200

    documented get_midi_files function

--- dataprocessing/find_midi_files.py
+++ dataprocessing/find_midi_files.py
@@ -1,13 +1,18 @@
+from typing import List
 from pathlib import Path
 
-def get_midi_files(path: str):
-    midi_file_search_path = Path(path)
-    midi_file_paths = (midi_file_search_path.rglob("*.mid"))
+
+def get_midi_files(folder_path: str) -> List[str]:
+    """Gets a list of midi file paths in the given folder, recursively"""
+
+    midi_file_search_path = Path(folder_path)
+    midi_file_paths = [str(midi_file_path.absolute()) for midi_file_path in midi_file_search_path.rglob("*.mid")]
 
     return midi_file_paths
+
 
 if __name__ == "__main__":
 
     midi_files = get_midi_files("./midi_filer")
     for midi_file in midi_files:
-        print(str(midi_file))
+        print(midi_file)
