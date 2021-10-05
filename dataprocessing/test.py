6d9dac49be38b740782f18536c2f93ecbae31755
Theodor Forgaard <tforgaard@gmail.com>
2021-10-05 20:57:17 +0200

    minor fixes

--- dataprocessing/test.py
+++ dataprocessing/test.py
@@ -1,24 +1,23 @@
-
 from music21 import *
 from music21 import converter, instrument, midi, note
 from find_midi_files import get_midi_files
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-from PIL import Image
+
 
 NUM_NOTES = 128
+Q_RATIO = 4
 
 if __name__ == "__main__":
     midi_files = get_midi_files("../midi_filer")
-    midi_file = midi_files[4]
+    midi_file = midi_files[0]
     print(midi_file)
     file = converter.parse(midi_file)
-
 
     instruments = file.parts
     time_signs = []
     durations = []
+
     for instrument in instruments:
         print(instrument.partName)
 
@@ -28,7 +27,7 @@ if __name__ == "__main__":
     print(time_signs)
     print(durations)
 
-    time_steps = int(durations[0].quarterLength * 4)
+    time_steps = int(durations[0].quarterLength * Q_RATIO)
     test_arr = np.zeros((time_steps, NUM_NOTES))
 
     for instr in instruments:
@@ -41,29 +40,23 @@ if __name__ == "__main__":
             if type(thisNote) == note.Note:
                 pitch = thisNote.pitch.midi
 
-                stop_ind = arr_ind + int(duration.quarterLength * 4)
+                stop_ind = arr_ind + int(duration.quarterLength * Q_RATIO)
                 test_arr[arr_ind:stop_ind, pitch] = 1
                 arr_ind = stop_ind
+
             if type(thisNote) == chord.Chord:
-                # print(thisNote)
-                stop_ind = arr_ind + int(duration.quarterLength * 4)
+                stop_ind = arr_ind + int(duration.quarterLength * Q_RATIO)
+
                 for n in thisNote.notes:
                     pitch = n.pitch.midi
                     test_arr[arr_ind:stop_ind, pitch] = 1
 
                 arr_ind = stop_ind
+
             else:
-                arr_ind += int(duration.quarterLength * 4)
+                arr_ind += int(duration.quarterLength * Q_RATIO)
 
-            # print(thisNote.fullName)
-            # print(thisNote.duration)
-
-    # img = Image.fromarray(test_arr, '1')
-    # img.save('my.png')
-    # img.show()
     plt.imshow(test_arr.T, interpolation="none")
     plt.show()
 
-    file.plot()
-
-
+    # file.plot()
