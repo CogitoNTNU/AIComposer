6ca0e8ff85349f1f2c228941d71b14c193d2723c
studBrage <brageher@stud.ntnu.no>
2021-10-12 18:44:54 +0200

    switched code from midi_to_numpy over to midi_checks

--- dataprocessing/function_testing.py
+++ dataprocessing/function_testing.py
@@ -1,6 +1,15 @@
 from midi_to_numpy import convert_file
 from numpy_to_midi import numpy_to_midi
 
-test_arr = convert_file("\midi_filer\Zelda_Overworld.mid")
+first_arr = convert_file("..\midi_filer\Zelda_Overworld.mid")
 
+numpy_to_midi(first_arr)
 
+second_arr = convert_file("test_output.mid")
+
+comparison = first_arr == second_arr
+equal_arrays = comparison.all()
+
+note_comp = comparison[:,:,0]
+
+print("DONE")
