9b556e12e4a0cba933e2e0078da55c356cfc2692
espen <espenbfo@stud.ntnu.no>
2021-10-26 19:21:01 +0200

    Fix sequence length error

--- models/training.py
+++ models/training.py
@@ -8,14 +8,13 @@ from tensorflow.keras.callbacks import ModelCheckpoint
 def train(data_filepath, epochs=1, model_filename="model"):
     gen = generator("data.h5", batch_size=BATCH_SIZE)
 
-    model = get_model(input_shape=(SEQUENCE_LENGTH, NUM_NOTES*3), output_shape=(NUM_NOTES*3))
+    model = get_model(input_shape=(SEQUENCE_LENGTH-1, NUM_NOTES*3), output_shape=(NUM_NOTES*3))
 
     model_checkpoint_callback = ModelCheckpoint(
         filepath=MODELS_FOLDER,
         monitor='accuracy',
         mode='max',
         save_best_only=True,)
-
     model.fit(gen, epochs=epochs, steps_per_epoch=1000, batch_size=BATCH_SIZE, callbacks=[model_checkpoint_callback])
 
     os.makedirs(MODELS_FOLDER, exist_ok=True)
