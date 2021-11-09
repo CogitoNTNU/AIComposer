de74b25659be80832cab6fa91ec3d1869579ff53
Matsellingsen <matsel@stud.ntnu.no>
2021-11-09 19:17:50 +0100

    mergeconflict

--- dataprocessing/convert_all_midi_in_folder.py
+++ dataprocessing/convert_all_midi_in_folder.py
@@ -11,15 +11,9 @@ def convert_all_midi_files_in_folder(folder_path, output_filename="data", save_i
     paths = find_midi_files.get_midi_files(folder_path)
     numpy_song_list = []
     count = len(paths)
-<<<<<<< HEAD
-    print(count, "count")
-    for i, path in enumerate(paths):
-        print(i, "of",count, ": ", path)
-=======
     start_index = 2062
     for i, path in enumerate(paths[start_index:]):
         print(i+start_index, "of",count, ": ", path)
->>>>>>> master
         try:
             numpy_arr = midi_to_numpy.convert_file(path, save_note_image=True)
             if save_individually:
@@ -42,7 +36,7 @@ def convert_midi_file(path,  output_filename="data"):
 
         with open(os.path.join(output_filename, str(Path(os.path.basename(path)).with_suffix(".h5"))), "wb") as f:
             np.save(f, numpy_arr)
-
+    
     except Exception as e:
         print(e)
 
